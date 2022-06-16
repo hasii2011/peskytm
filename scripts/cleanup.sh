5b9d1bae590f92202261d460955fe0dc949fb183
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-06-16 10:23:41 -0500

    The first commit🎉

---
+++ scripts/cleanup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function changeToProjectRoot {
+
+    areHere=$(basename "${PWD}")
+    export areHere
+    if [[ ${areHere} = "scripts" ]]; then
+        cd ..
+    fi
+}
+
+changeToProjectRoot
+
+rm -rf dist build
+
+rm -rf peskytm.egg-info
+rm -rf .eggs
+
---
+++ scripts/packageme.sh
@@ -0,0 +1,36 @@
+#!/usr/local/bin/bash
+
+function changeToProjectRoot {
+
+    areHere=$(basename "${PWD}")
+    export areHere
+    if [[ ${areHere} = "scripts" ]]; then
+        cd ..
+    fi
+}
+
+changeToProjectRoot
+
+#
+#  Assumes python 3 is on PATH
+#
+clear
+
+if [[ $# -eq 0 ]] ; then
+        echo "in alias mode"
+        rm -rf build dist
+        # python setup.py py2app -A --iconfile src/org/pyut/resources/img/Pyut.icns
+        python setup.py py2app
+else
+    if [[ ${1} = 'deploy' ]] ; then
+            echo "in deploy mode"
+            rm -rf build dist
+            # python -O setup.py py2app --packages=wx,xmlschema,pygmlparser --iconfile src/org/pyut/resources/img/Pyut.icns
+            python -O setup.py py2app --packages=click
+            echo "remove invalid link that code signing complains about"
+            cd "dist/unmounttm.app/Contents/Resources/lib/python3.9"  || ! echo "No such directory"
+            rm -rfv site.pyo
+    else
+        echo "Unknown command line arguments"
+    fi
+fi
